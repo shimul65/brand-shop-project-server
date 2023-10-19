a966294374ca6fb495954953a23acea57ad93e4f
Shimul <shimulmohammad420@gmail.com>
2023-10-20 04:34:11 +0600

    get single product using its id applying get method and findOne query

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 
 const port = process.env.PORT || 5555;
@@ -32,6 +32,13 @@ async function run() {
 
         app.get('/products', async (req, res) => {
             const result = await productsCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await productsCollection.findOne(query);
             res.send(result);
         })
 
